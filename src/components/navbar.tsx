4bfa563cada7b83ddc3358ada100e32a190d1a1c
Muzaffar Yusupov <153423021+muzaffaryusupov1@users.noreply.github.com>
2025-05-16 16:52:12 +0500

    Hero and Product section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,13 @@ const Navbar = () => {
 	return (
 		<header className='flex items-center px-4 md:px-12 py-2 justify-between fixed top-0 w-full z-50 shadow bg-white'>
 			<Link href={'/'}>
-				<Image src={'/site-logo.png'} alt='site logo' width={150} height={40} />
+				<Image
+					src={'/site-logo.png'}
+					alt='site logo'
+					width={150}
+					height={40}
+					style={{ width: 'auto', height: 'auto' }}
+				/>
 			</Link>
 
 			<div className='flex items-center space-x-2.5 text-sm'>
